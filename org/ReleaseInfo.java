7f345e6eec37c734eaae5a0bfdb142b30026fb57
klukas <klukas>
2008-10-14 08:09:33 +0000

    *** empty log message ***

--- org/ReleaseInfo.java
+++ org/ReleaseInfo.java
@@ -73,7 +73,7 @@ public class ReleaseInfo {
 					return true;
 				break;
 			case MetaCrop_ACCESS :
-				if (currentRelease!=Release.KGML_EDITOR && currentRelease==Release.RELEASE_IPK)
+				if (currentRelease!=Release.KGML_EDITOR)
 					return true;
 				break;	
 			case DATA_CARD_ACCESS :
