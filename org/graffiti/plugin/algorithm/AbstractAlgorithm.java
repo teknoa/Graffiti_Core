67fbbbe392389c7755949cbae6db61c0de59f051
klukas <klukas>
2009-05-07 07:06:35 +0000

    *** empty log message ***

--- org/graffiti/plugin/algorithm/AbstractAlgorithm.java
+++ org/graffiti/plugin/algorithm/AbstractAlgorithm.java
@@ -5,7 +5,7 @@
 //   Copyright (c) 2001-2004 Gravisto Team, University of Passau
 //
 //==============================================================================
-// $Id: AbstractAlgorithm.java,v 1.1 2007/05/31 12:55:54 klukas Exp $
+// $Id: AbstractAlgorithm.java,v 1.2 2009/05/07 07:06:35 klukas Exp $
 
 package org.graffiti.plugin.algorithm;
 
@@ -47,7 +47,7 @@ public abstract class AbstractAlgorithm implements Algorithm {
 	}
 
 	/**
-	 * Default: no accerlator for the menu item, created for this algorithm.
+	 * Default: no accelerator for the menu item, created for this algorithm.
 	 */
 	public KeyStroke getAcceleratorKeyStroke() {
 		return null;
@@ -125,6 +125,14 @@ public abstract class AbstractAlgorithm implements Algorithm {
 
 	public void setActionEvent(ActionEvent a) {
 		actionEvent = a;
+	}
+
+	public void execute() {
+		throw new RuntimeException("NOT IMPLEMENTED (OVERRIDE METHOD)");
+	}
+
+	public String getName() {
+		throw new RuntimeException("NOT IMPLEMENTED (OVERRIDE METHOD)");
 	}
 }
 
