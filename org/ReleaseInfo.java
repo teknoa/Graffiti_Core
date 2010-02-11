958f87229330ef09580c03e95a0e386cb7dac6e5
klukas <klukas>
2010-02-11 11:37:33 +0000

    *** empty log message ***

--- org/ReleaseInfo.java
+++ org/ReleaseInfo.java
@@ -11,7 +11,6 @@ package org;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.Writer;
